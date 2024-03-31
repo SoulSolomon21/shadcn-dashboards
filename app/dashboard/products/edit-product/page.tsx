70769e3b581431755fda6be6801271b4b3318f9c
Sekamatte Soul Solomon <99466179+SoulSolomon21@users.noreply.github.com>
2024-03-31 03:12:51 +0300

    add files

--- app/dashboard/products/edit-product/page.tsx
+++ app/dashboard/products/edit-product/page.tsx
@@ -70,7 +70,7 @@ import {
     TooltipTrigger,
 } from "@/components/ui/tooltip"
 
-export function Dashboard() {
+function EditProduct() {
     return (
         <div className="flex min-h-screen w-full flex-col bg-muted/40">
             <aside className="fixed inset-y-0 left-0 z-10 hidden w-14 flex-col border-r bg-background sm:flex">
@@ -598,3 +598,5 @@ export function Dashboard() {
         </div>
     )
 }
+
+export default EditProduct
